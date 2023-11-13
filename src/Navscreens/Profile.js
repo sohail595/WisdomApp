179ee8d191e4a364ac87d5ec58b0fc3177a5a8c0
sohail595 <86399479+sohail595@users.noreply.github.com>
2023-11-13 22:03:03 +0500

    Update Profile.js

--- src/Navscreens/Profile.js
+++ src/Navscreens/Profile.js
@@ -1,39 +1,3 @@
-// import { SafeAreaView, View, Text, Pressable, Image } from "react-native";
-// import tailwind from "twrnc";
-// import Ionicons from "@expo/vector-icons/Ionicons";
-// import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-// import { Avatar } from "@/components/avatar";
-
-// export const UserProfile = () => {
-//   return (
-//     <SafeAreaView style={tailwind`flex-1 w-full bg-gray-950`}>
-//       <View style={tailwind`flex-1 items-center justify-center gap-8`}>
-//         <Avatar source={{ uri: "https://source.unsplash.com/random" }} />
-//         <View style={tailwind`gap-2 items-center`}>
-//           <Text style={tailwind`text-gray-50 text-3xl font-bold`}>
-//             Joe Bloggs
-//           </Text>
-//           <Text style={tailwind`text-gray-50 text-lg`}>joe@bloggs.com</Text>
-//         </View>
-//       </View>
-//       <View style={tailwind`flex-1 justify-center gap-8`}>
-//         <Pressable style={tailwind`flex-row items-center gap-2 px-8`}>
-//           <Ionicons name="settings-outline" size={24} color="#fff" />
-//           <Text style={tailwind`text-gray-50 text-lg`}>Settings</Text>
-//         </Pressable>
-//         <Pressable style={tailwind`flex-row items-center gap-2 px-8`}>
-//           <Ionicons name="help-buoy-outline" size={24} color="#fff" />
-//           <Text style={tailwind`text-gray-50 text-lg`}>Help</Text>
-//         </Pressable>
-//         <Pressable style={tailwind`flex-row items-center gap-2 px-8`}>
-//           <MaterialIcons name="logout" size={24} color="#fff" />
-//           <Text style={tailwind`text-gray-50 text-lg`}>Logout</Text>
-//         </Pressable>
-//       </View>
-//     </SafeAreaView>
-//   );
-// };
-
 import React from 'react';
 import {View, ScrollView, Image, Text, TouchableOpacity} from 'react-native';
 
